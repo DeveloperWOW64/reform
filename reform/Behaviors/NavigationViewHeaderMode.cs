478c0b3d424befb014cb02ac27b6015ab08b47f2
Jupiter <71598437+DeveloperWOW64@users.noreply.github.com>
2021-04-12 18:18:50 +0100

    FINISH THE FIRST RELEASE

---
+++ reform/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace reform.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
---
+++ reform/Services/Ink/UndoRedo/IUndoRedoOperation.cs
@@ -0,0 +1,9 @@
+﻿namespace reform.Services.Ink.UndoRedo
+{
+    public interface IUndoRedoOperation
+    {
+        void ExecuteUndo();
+
+        void ExecuteRedo();
+    }
+}
